7b6d92e7f6faadc9c23216427964b7a053793934
khinmarwin087-oss <khinmarwin087@gmail.com>
2026-02-18 23:51:13 +0630

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   base: "./",
   resolve: {
     alias: {
-      // မင်းက root မှာပဲ ထားတာဆိုတော့ @ ကို လက်ရှိ folder လို့ သတ်မှတ်ပေးရမယ်
-      "@": "/",
+      "@": path.resolve(__dirname, "./"),
     },
   },
 });
